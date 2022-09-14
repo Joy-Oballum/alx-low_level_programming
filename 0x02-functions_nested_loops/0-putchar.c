9f4854d94c18bc3a6bf52763f94cfbdd2534bf4e
Joy-Oballum <joyoballum@yahoo.com>
2022-09-13 17:46:17 -0700

    nested if

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Print _putchar followed by a newline
+ * Return: On Success 1
+ * On error, -1 is returned, and errno is set appropriately
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
